af056058227440797b5bf4cf0034a0c53570a691
aud404_st <aud404_st@CR404-PC12>
2023-04-19 17:28:18 +0300

    Добавьте файлы проекта.

---
+++ linq_04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Immutable;
+using System.Text;
+
+namespace Requests
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Random random = new();
+            var array = new[] { random.Next(1, 5), random.Next(1, 5), random.Next(1, 5), random.Next(1, 5), random.Next(1, 5) };
+
+            Console.WriteLine("початковий список:");
+            foreach(int num in array)
+            {
+                Console.WriteLine($"{num} ");
+            }
+
+            var res = array.Distinct();
+            res = res.ToImmutableSortedSet();
+
+            Console.WriteLine("\nрезультат:");
+            foreach (int num in res)
+            {
+                Console.WriteLine($"{num} ");
+            }
+        }
+    }
+}
